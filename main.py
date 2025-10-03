1cef16152d7f9c32921a5e4b77e1792d2066dc0c
Lee Albert <koresaremi2001@gmail.com>
2025-10-03 18:48:13 +0600

    help

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('hello world')
+import flet as ft 
+
+def main(page: ft.Page):
+    page.title = 'My first app!'
+
+    greeting_text = ft.Text("Hello world!")
+
+    page.add(greeting_text)
+
+    
+
+
+
+ft.app(target = main, view = ft.WEB_BROWSER)
+
