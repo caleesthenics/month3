b66a5aaf3f0a2057932c09ade1e35ede892f82ca
Lee Albert <koresaremi2001@gmail.com>
2025-10-10 18:25:24 +0600

    ne

--- main.py
+++ main.py
@@ -8,11 +8,21 @@ def main(page: ft.Page):
 
     greeting_text = ft.Text("Hello world!")
 
-    name_input = ft.TextField(label = 'vvedite imya:')
-
-    age_input = ft.TextField(label = 'vvedite vozrast')
 
     error_text = ft.Text()
+
+    gr_history = []
+
+    # def update_history():
+    #     history_controls = [ft.Text('istoriya privetstvii')]
+
+    history_text = ft.Text(f'Istoriya privetstvii:')
+
+
+
+
+
+
 
     def button(_):
         name = name_input.value.strip()
@@ -45,13 +55,18 @@ def main(page: ft.Page):
                 name_input.value = None
                 age_input.value = None
                 error_text.value = None
+            
+            gr_history.append(f'{time.strftime("%Y-%m-%d %H:%M")} - {name}')
+            history_text.value = 'istoriya privetstviya:\n' + '\n'.join(gr_history) 
 
         else:
             error_text.value = 'Pole name ili age ne zapolneno'
         
         page.update()
 
-    
+    name_input = ft.TextField(label = 'vvedite imya:',on_submit = button)
+    age_input = ft.TextField(label ='vvedite vozrast',on_submit = button)
+
     def theme_button(_):
         if page.theme_mode == ft.ThemeMode.LIGHT:
             page.theme_mode = ft.ThemeMode.DARK
@@ -59,12 +74,22 @@ def main(page: ft.Page):
             page.theme_mode = ft.ThemeMode.LIGHT
         page.update()
 
+    def history_remove(_):
+        gr_history.clear
+        history_text.value = 'Istoriya privetstviya'
+        greeting_text.value = 'Hello World!'
+        page.update()
+        
 
-    knopka2 = ft.IconButton(icon = ft.Icons.BRIGHTNESS_7,on_click = theme_button)
-                            
+    knopka2 = ft.IconButton(icon = ft.Icons.SUNNY,on_click = theme_button)
+
+    knopka3 = ft.Button('remove', on_click = history_remove)
+
     knopka = ft.Button('send', on_click = button) 
 
-    page.add(greeting_text, name_input,age_input, knopka,knopka2, error_text)
+    page.add(ft.Row([greeting_text], alignment = ft.MainAxisAlignment.CENTER),name_input,age_input,
+             ft.Row([knopka,knopka2,knopka3],alignment= ft.MainAxisAlignment.CENTER),error_text,history_text
+    )
 
 
 ft.app(target = main)
